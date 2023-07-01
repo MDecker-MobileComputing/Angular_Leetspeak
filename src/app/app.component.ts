ffd30bf0b45a189b29b1034332b0968afe34720a
MDecker-MobileComputing <mide_42b@yahoo.de>
2023-07-01 13:41:22 +0200

    Two-Way-Binding fuer <input type="text" ...> funktioniert

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'LeetSpeak';
+
+  /**
+   * Diese Member-Variable ist mit einem Two-Way-Binding an das <textarea>-Element
+   * für die Eingabe des zu übersetzenden Textes gebunden.
+   */
+  public eingabeText = "blafasel";
+
+  /**
+   * Event-Handler-Methode für Button "Nach Leetspeek übersetzen".
+   */
+  public onUebersetzenButton() {
+
+    console.log("Übersetzen-Button gedrückt: " + this.eingabeText);
+  }
+
+  /**
+   * Event-Handler-Methode für Button "Eingabefeld löschen".
+   */
+  public onLoeschenButton() {
+
+    console.log("Lösch-Button gedrückt.");
+    this.eingabeText = "";
+  }
+
 }
