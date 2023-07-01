924d48cf3ec691093034ffc427db705f2644af0d
MDecker-MobileComputing <mide_42b@yahoo.de>
2023-07-01 13:59:13 +0200

    Textausgabefeld ist jetzt auch da, aber Algo noch nicht implementiert

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,14 +11,20 @@ export class AppComponent {
    * Diese Member-Variable ist mit einem Two-Way-Binding an das <textarea>-Element
    * für die Eingabe des zu übersetzenden Textes gebunden.
    */
-  public eingabeText = "blafasel";
+  public eingabeText = "";
+
+    /**
+   * Diese Member-Variable ist mit einem Two-Way-Binding an das <textarea>-Element
+   * für die Ausgabe des zu übersetzten Textes gebunden.
+   */
+  public ausgabeText = "";
 
   /**
    * Event-Handler-Methode für Button "Nach Leetspeek übersetzen".
    */
   public onUebersetzenButton() {
 
-    console.log("Übersetzen-Button gedrückt: " + this.eingabeText);
+    this.ausgabeText = this.eingabeText.trim();
   }
 
   /**
