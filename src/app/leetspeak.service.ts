c5ffc257bf1e40ca03d439fd617267d6125ff1b7
MDecker-MobileComputing <mide_42b@yahoo.de>
2023-07-02 20:12:24 +0200

    leetspeak.service.ts: Refactoring

--- src/app/leetspeak.service.ts
+++ src/app/leetspeak.service.ts
@@ -13,15 +13,15 @@ export class LeetspeakService {
 
   public ubersetzteNachLeetspeak(eingabeText: string): string {
 
-    return eingabeText.replace(/a/gi, "4")
-                      .replace(/b/gi, "8")
-                      .replace(/e/gi, "3")
-                      .replace(/g/gi, "9")
-                      .replace(/i/gi, "1")
-                      .replace(/l/gi, "1") // auch "i" wird schon auf Ziffer "1" abgebildet
-                      .replace(/o/gi, "0")
-                      .replace(/s/gi, "5")
-                      .replace(/z/gi, "2");
+    return eingabeText.replace( /a/gi , "4" )
+                      .replace( /b/gi , "8" )
+                      .replace( /e/gi , "3" )
+                      .replace( /g/gi , "9" )
+                      .replace( /i/gi , "1" )
+                      .replace( /l/gi , "1" ) // auch "i" wird schon auf Ziffer "1" abgebildet
+                      .replace( /o/gi , "0" )
+                      .replace( /s/gi , "5" )
+                      .replace( /z/gi , "2" );
   }
 
 }
